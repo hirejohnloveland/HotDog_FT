295814d062f0c601bd7f8a905b0786fe1dad9a1c
hirejohnloveland <hirejohnloveland@gmail.com>
2021-06-17 18:41:55 -0500

    Netlify #19

--- src/components/menuitem.jsx
+++ src/components/menuitem.jsx
@@ -6,26 +6,31 @@ class MenuItem extends Component {
     const item = this.props.item;
 
     return (
-      <div className="card mb-3 w-100">
-        <div className="row g-0">
-          <div className="col-md-4">
-            <img
-              className="card-img-top img-responsive"
-              src={item.image}
-              alt="..."
-            />
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h3 className="card-title mt-3">{item.name}</h3>
-              <h5 className="card-text mt-3">{item.desc}</h5>
-              <h5 className="card-text mt-3">{item.price}</h5>
-              <button
-                onClick={() => this.props.onAdd(item.id)}
-                className="btn btn-primary mt-3"
-              >
-                Add to Cart
-              </button>
+      <div className="container">
+        <div className="card mb-3 w-100">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                className="card-img-top img-responsive"
+                src={item.image}
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div class="card-header">
+                <h3 className="card-title">{item.name}</h3>
+              </div>
+              <div className="card-body my-auto">
+                <h5 className="card-text my-auto">{item.desc}</h5>
+              </div>
+            
+                <button
+                  onClick={() => this.props.onAdd(item.id)}
+                  className="btn btn-primary m-2"
+                >
+                  Add to Cart for {item.price}
+                </button>
+             
             </div>
           </div>
         </div>
